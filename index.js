2a9c9b9d6fe1db3c168467002cfaf2e98f3f577f
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-05-25 11:33:20 +0100

    add error block if invalid url provided

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const dns = require("dns");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
@@ -25,8 +26,25 @@ app.get('/api/hello', function(req, res) {
 // Second API endpoint 
 app.post('/api/shorturl', function (req, res){
   const providedUrl = req.body.url
+// lookup the hostname passed as argument
+  dns.lookup(providedUrl, (error, address, family) => {
+  // if an error occurs, eg. the hostname is incorrect!
+  if (error) {
+    res.json({
+      error: "Invalid Hostname"
+    });
+  } else {
+    // if no error exists
+    console.log(
+      `The ip address is ${address} and the ip version is ${family}`
+    );
+    res.json({
+      
+    })
+  }
+});
   console.log(providedUrl);
-  res.json({
+    res.json({
     URL : providedUrl
   })
 
