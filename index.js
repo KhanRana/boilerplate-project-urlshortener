8432af13912a21a39f7eb42ff6758a2f9b746fb2
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-05-27 15:22:52 +0100

    verify url and hostname both to verify url

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { URL } = require("url");
 const dns = require("dns");
+const { hostname } = require("os");
 const app = express();
 
 // Basic Configuration
@@ -37,30 +38,33 @@ const isValidUrl = (urlString) => {
 // Second API endpoint
 app.post("/api/shorturl", function (req, res) {
   let providedUrl = req.body.url;
-  console.log(providedUrl);
-  if (isValidUrl(providedUrl) === false) {
+  if (isValidUrl(providedUrl)) {
+    // lookup the hostname passed as argument
+    const validataHost = new URL(providedUrl);
+    const newHost = validataHost.hostname;
+    // console.log(newHost); 
+    dns.lookup(newHost, (error, address, family) => {
+      // if an error occurs, eg. the hostname is incorrect!
+      if (error) {
+        res.json({
+          error: "Invalid url",
+        });
+      } else {
+        // if no error exists
+        console.log(`The ip address is ${address} and the ip version is ${family}`);
+
+        if (!urls.includes(providedUrl)) {
+          urls.push(providedUrl);
+        }
+        res.json({
+          original_url: providedUrl,
+          short_url: urls.indexOf(providedUrl),
+        });
+      }
+    });
+  } else {
     res.json({
       error: "Invalid url",
-    });
-  }
-  // lookup the hostname passed as argument
-  // dns.lookup(providedUrl, (error, address, family) => {
-  //   // if an error occurs, eg. the hostname is incorrect!
-  //   if (error) {
-  //     res.json({
-  //       error: "Invalid url",
-  //     });
-  //   }
-  else {
-    // if no error exists
-    // console.log(`The ip address is ${address} and the ip version is ${family}`);
-
-    if (!urls.includes(providedUrl)) {
-      urls.push(providedUrl);
-    }
-    res.json({
-      original_url: providedUrl,
-      short_url: urls.indexOf(providedUrl),
     });
   }
 });
